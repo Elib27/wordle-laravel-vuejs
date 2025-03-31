b7aa21c899e23911fe05495afe5222b0be2ff647
Eliot Bas <eliot.bas@pi.esisar.grenoble-inp.fr>
2025-03-31 14:16:55 +0200

    fix controller

--- app/Http/Controllers/WordleController.php
+++ app/Http/Controllers/WordleController.php
@@ -14,20 +14,24 @@ enum LetterStatus: string
 
 class WordleController extends Controller
 {
+    const WORD_LENGTH = 5;
 
     public function getRandomWordId(Request $request) {
         $word = Word::all()->random();
         return response()->json(['id' => $word->id], 200);
     }
 
-    const WORD_TO_GUESS = 'apple';
-    const WORD_LENGTH = 5;
-
     public function checkWord(Request $request)
     {
 
         $id = intval($request->route('id'));
         $guess = $request->input('guess');
+
+        $wordToGuess = Word::find($id)?->word;
+
+        if ($wordToGuess === null) {
+            return response()->json(['message' => "the requested id doesn't exist, get one with the /api/randomwordid endpoint"], 400);
+        }
 
         if (strlen($guess) !== self::WORD_LENGTH) {
             return response()->json(['message' => 'the length of the guessed word should be 5'], 400);
@@ -38,10 +42,10 @@ class WordleController extends Controller
 
         // Detection of well placed letters (correct)
         for ($i = 0; $i < self::WORD_LENGTH; $i++) {
-            if ($guess[$i] === self::WORD_TO_GUESS[$i]) {
+            if ($guess[$i] === $wordToGuess[$i]) {
                 $guessResult[$i] = LetterStatus::CORRECT_POSITION;
             } else {
-                $pendingLetters[self::WORD_TO_GUESS[$i]] = ($pendingLetters[self::WORD_TO_GUESS[$i]] ?? 0) + 1;
+                $pendingLetters[$wordToGuess[$i]] = ($pendingLetters[$wordToGuess[$i]] ?? 0) + 1;
             }
         }
 
